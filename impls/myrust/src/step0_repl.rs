15889e0d78eb8c7b737b1dfdfe2d0a31b68802bc
Akira Ishino <akira.ishino@gmail.com>
2023-07-01 22:56:25 +0900

    step0_repl

---
+++ impls/myrust/src/step0_repl.rs
@@ -0,0 +1,40 @@
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        match read() {
+            Some(input) => {
+                let ret = eval(&input[..]);
+                print(&ret[..]);
+            },
+            None => break,
+        }
+    }
+}
+
+fn read() -> Option<String> {
+    print!("user> ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n_bytes) => {
+            if n_bytes == 0 {
+                None
+            } else {
+                Some(input)
+            }
+        },
+        Err(e) => {
+            writeln!(io::stderr(), "error: {}", e).unwrap();
+            None
+        },
+    }
+}
+
+fn eval(s: &str) -> String {
+    s.to_string()
+}
+
+fn print(s: &str) {
+    println!("{}", s);
+}
