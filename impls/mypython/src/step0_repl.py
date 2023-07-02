56fefcedfe3b3a2a9557060f9a8f2c74eda90896
Akira Ishino <akira.ishino@gmail.com>
2023-07-02 15:50:19 +0900

    mypython step0

---
+++ impls/mypython/src/step0_repl.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def mal_read() -> str:
+    return input("user> ")
+
+
+def mal_eval(exp: str) -> str:
+    return exp
+
+
+def mal_print(exp: str) -> str:
+    print(exp)
+
+
+def mal_quit():
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    while True:
+        s = ""
+        try:
+            s = mal_read()
+        except EOFError:
+            mal_quit()
+        ret = mal_eval(s)
+        mal_print(ret)
